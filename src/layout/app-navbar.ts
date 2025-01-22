48752bd1154fe04ce9ea1827b6108ea331383185
Chen Peleg <chenpeleg@gmail.com>
2025-01-22 22:23:56 +0200

    added menu

--- src/layout/app-navbar.ts
+++ src/layout/app-navbar.ts
@@ -2,7 +2,7 @@ import {html, LitElement} from 'lit'
 import {customElement} from 'lit/decorators.js'
 import {globalStyleSheet} from '../styles/global-style-sheet.ts';
 import { Txt} from '../translations/translations.ts';
-
+  import menuButton from '../assets/svg/menu-button.svg'
 
 @customElement('app-navbar')
 export class AppNavbar extends LitElement {
@@ -13,9 +13,11 @@ export class AppNavbar extends LitElement {
     render() {
         return html`
             <nav class="w-full h-12 bg-gray-100 shadow  relative"> 
-                <div class="h-full flex flex-row shadow-md">
-                    <button>
-                        
+                <div class="h-full flex flex-row shadow  gap-3">
+                    <button class="h-full flex flex-col justify-center items-center" style="  border: none"> 
+                        <a href="#abc"  >
+                            <img src=${menuButton} class=" " alt="menu"/>
+                        </a> 
                     </button>
                     <div class="flex flex-row justify-center items-center">
                         ${Txt.attendance} ${Txt.pistachio}
