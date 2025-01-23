0fe4fd2b21807be04b8988f76802d34788f079a4
Chen Peleg <chenpeleg@gmail.com>
2025-01-23 15:22:04 +0200

    added version changes 2

--- src/components/app-child.ts
+++ src/components/app-child.ts
@@ -5,25 +5,37 @@ import {ChildStatus} from '../models/ChildStatus.ts';
 
 @customElement('app-child')
 export class AppChild extends LitElement {
-    @property({type: Object})
-    child: ChildStatus | null = null;
+    @property({type: Object}) child: ChildStatus | null = null;
 
-    @property({type: Function})
-    onClick: () => void = () => {};
+    @property({type: Function}) onClick: () => void = () => {
+    };
 
     firstUpdated() {
         (this.shadowRoot as ShadowRoot).adoptedStyleSheets = [globalStyleSheet];
     }
 
-
     render() {
         return html`
-            <div id="child-root" class="w-full h-12 w-full shadow  relative bg-red-100 gap-3"> 
-                    <button @click="${this.onClick}" class="h-full w-full flex flex-row justify-start items-center" style="  border: none">
-                        ${this.child?.name} ${this.child?.checkedIn ? '1' : '0'}
-                    </button>  
-            </div>
+            <button @click="${this.onClick}"
+                    class=" flex flex-row justify-start items-center h-12 w-full shadow  relative  gap-3 rounded-md   "
+                    style="  border: none">
+                    <span class="w-5 h-5 relative">
+                    <span id="check-mark-container" class="absolute top-0">
+                        ${this.getCheckMark()} 
+                    </span>
+                </span>
+                <span class="flex flex-row justify-center items-center">
+                    ${this.child?.name} ${this.child?.checkedIn ? '1' : '0'}
+                   </span>
+            </button>
+
         `
+    }
+
+    private getCheckMark() {
+
+        return this.child?.checkedIn ? html`
+            <check-mark </check-mark>` : html``
     }
 }
 
