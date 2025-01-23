540151d75f62a3759223d91c3045dd265fd2f3d1
Chen Peleg <chenpeleg@gmail.com>
2025-01-23 16:42:25 +0200

    changed font size 24

--- src/styles/global-custom-css.ts
+++ src/styles/global-custom-css.ts
@@ -1,3 +1,6 @@
 export const globalCustomCss = `
-  
+    *, button {
+        font-size: 24px;
+        font-family: "Noto Sans Hebrew", Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    }
     `
