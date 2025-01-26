94287fbc043b33131b6e7ba8b9f104ef3078849a
Chen Peleg <chenpeleg@gmail.com>
2025-01-26 15:30:41 +0200

    fixed direction 2

--- src/styles/global-style-sheet.ts
+++ src/styles/global-style-sheet.ts
@@ -4,13 +4,16 @@ import {css, unsafeCSS} from 'lit';
 import tailwindStyles from './tailwind-utils.css?inline';
 import customComponent from './special-components.css?inline';
 import themeStyles from './theme-styles.css?inline';
+import scrollBarStyles from '../css/scroll-bar.css?inline';
 
 const cssTailwind = unsafeCSS(tailwindStyles)
 const cssCustomGlobal = unsafeCSS(customComponent)
 export const allComputedStyles = css`
     ${unsafeCSS(cssCustomGlobal)}
     ${unsafeCSS(cssTailwind)}
-    ${unsafeCSS(themeStyles)}`;
+    ${unsafeCSS(themeStyles)}
+    ${unsafeCSS(scrollBarStyles)}
+`;
 
 
 export const globalStyleSheet = new CSSStyleSheet();
