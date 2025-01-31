d62c98515390d292250e6339fe0fa8c2da644374
Chen Peleg <chenpeleg@gmail.com>
2025-01-31 15:50:32 +0200

    ci works

--- src/services/Configuration.service.ts
+++ src/services/Configuration.service.ts
@@ -18,6 +18,7 @@ export class ConfigurationService extends AbstractBaseService {
     }
     private getEnvironmentFromEnvVar() {
         const envVar =  import.meta.env.VITE_MODE_NAME;
+        console.log(envVar)
         switch (envVar) {
             case 'development':
                 return Environment.Development;
