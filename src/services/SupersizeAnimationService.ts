e50d5d3db0d111b9b2523995e6ec921df7376211
Chen Peleg <chenpeleg@gmail.com>
2025-02-06 19:44:42 +0200

    added

--- src/services/SupersizeAnimationService.ts
+++ src/services/SupersizeAnimationService.ts
@@ -15,6 +15,22 @@ export class SupersizeAnimationService extends AbstractBaseService {
             confetti.stop();
         }, 3000)
     }
+    showEmojiJumping() {
+
+        const emojiContainer = document.createElement('div');
+        emojiContainer.classList.add('emoji-container');
+        const emoji = document.createElement('div');
+        emoji.classList.add('emoji-surprise');
+        emojiContainer.appendChild(emoji);
+        document.body.appendChild(emojiContainer);
+        emoji.innerHTML = '🎉';
+        if (emojiContainer) {
+            emojiContainer.classList.add('jumping');
+            setTimeout(() => {
+                emojiContainer.classList.remove('jumping');
+            }, 3000)
+        }
+    }
 
 
 }
