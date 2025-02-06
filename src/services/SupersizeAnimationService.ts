82b8cdebfd66b940ae2e9d7b8b2dc22beb63e2da
Chen Peleg <chenpeleg@gmail.com>
2025-02-06 20:07:48 +0200

    animation works

--- src/services/SupersizeAnimationService.ts
+++ src/services/SupersizeAnimationService.ts
@@ -7,6 +7,7 @@ export class SupersizeAnimationService extends AbstractBaseService {
     constructor(provider: ServicesResolver) {
         super(provider);
     }
+
     showConfetti() {
 
         confetti.start();
@@ -15,19 +16,22 @@ export class SupersizeAnimationService extends AbstractBaseService {
             confetti.stop();
         }, 3000)
     }
+
     showEmojiJumping() {
 
         const emojiContainer = document.createElement('div');
         emojiContainer.classList.add('emoji-container');
-        const emoji = document.createElement('div');
-        emoji.classList.add('emoji-surprise');
-        emojiContainer.appendChild(emoji);
+        emojiContainer.innerHTML = `<div class="emoji-and-text">
+                                        <div class="emoji-surprise"> 🎉
+                                        </div>
+                                        <div class="text">Great!</div>
+                                     </div>`;
         document.body.appendChild(emojiContainer);
-        emoji.innerHTML = '🎉';
+
         if (emojiContainer) {
             emojiContainer.classList.add('jumping');
             setTimeout(() => {
-                emojiContainer.classList.remove('jumping');
+                // emojiContainer.remove()
             }, 3000)
         }
     }
