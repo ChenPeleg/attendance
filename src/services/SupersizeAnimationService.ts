675ee473b4ef971de36aa50b613634ab717bf66e
Chen Peleg <chenpeleg@gmail.com>
2025-02-06 20:09:20 +0200

    animation works

--- src/services/SupersizeAnimationService.ts
+++ src/services/SupersizeAnimationService.ts
@@ -16,13 +16,18 @@ export class SupersizeAnimationService extends AbstractBaseService {
             confetti.stop();
         }, 3000)
     }
+    private randomCuteEmoji() {
+        const cuteEmojis = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷', '🐸', '🐵', '🐔', '🐧', '🐦', '🐤', '🐥', '🦆', '🦅', '🦉', '🦇', '🐺', '🐗', '🐴', '🦄', '🐝', '🐛', '🦋', '🐌', '🐞', '🐜', '🦗', '🕷', '🦂', '🦟', '🦠', '🐢', '🐍', '🦎', '🦖', '🦕', '🐙', '🦑', '🦐', '🦞', '🦀', '🐡', '🐠', '🐟', '🐬', '🐳', '🐋', '🦈', '🐊', '🐅', '🐆', '🦓', '🦍', '🦧', '🦣', '🐘', '🦛', '🦏', '🐪', '🐫', '🦒', '🦘', '🦙', '🦌', '🦍', '🦝', '🦨', '🦡', '🦦', '🦥', '🦮', '🐕', '🦮', '🐕‍🦺', '🦤', '🦩']
+        return cuteEmojis[Math.floor(Math.random() * cuteEmojis.length)];
+
+    }
 
     showEmojiJumping() {
 
         const emojiContainer = document.createElement('div');
         emojiContainer.classList.add('emoji-container');
         emojiContainer.innerHTML = `<div class="emoji-and-text">
-                                        <div class="emoji-surprise"> 🎉
+                                        <div class="emoji-surprise"> ${this.randomCuteEmoji()}
                                         </div>
                                         <div class="text">Great!</div>
                                      </div>`;
