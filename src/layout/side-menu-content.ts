b25c21e67a5bd974c997e9869c599d2547c0ad38
Chen Peleg <chenpeleg@gmail.com>
2025-12-18 13:03:13 +0200

    Add app-root-layout-dialog component and conditional rendering for development environment

--- src/layout/side-menu-content.ts
+++ src/layout/side-menu-content.ts
@@ -7,8 +7,6 @@ import addChild from '../assets/svg/addchild.svg'
 
 import {globalStore} from '../store/Store.ts';
 import {ActionType} from '../models/AppAction.ts';
-import {ConfigurationService} from '../services/Configuration.service.ts';
-import {servicesProvider} from '../services/provider/ServicesProvider.ts';
 
 @customElement('side-menu-content')
 export class SideMenuContent extends LitElement {
@@ -32,13 +30,11 @@ export class SideMenuContent extends LitElement {
     }
 
     render() {
-        const env = servicesProvider.getService(ConfigurationService).environment
+
         return html`
             <div class="flex flex-col gap-4 ">
                 <div class="h-14 w-full flex-row flex justify-between gap-5">
-                    <div class="${env === 'production' ? 'hidden' : 'contents'}"> 
-                        <app-dialog></app-dialog>
-                    </div>
+                  
                
                     <span>
                     ${Txt.addChild}
--- src/root/app-root-layout-dialog.ts
+++ src/root/app-root-layout-dialog.ts
@@ -3,6 +3,8 @@ import {customElement} from 'lit/decorators.js'
 import {globalStyleSheet} from '../styles/global-style-sheet.ts';
 import '../main/app-main.ts'
 import '../layout/app-navbar.ts'
+import {servicesProvider} from '../services/provider/ServicesProvider.ts';
+import {ConfigurationService} from '../services/Configuration.service.ts';
 
 
 @customElement('app-root-layout-dialog')
@@ -14,12 +16,16 @@ export class AppRootLayoutDialog extends LitElement {
 
 
     render() {
-        return html` 
-                <div class="h-screen w-screen flex-col items-center justify-center  overflow-x-hidden"
-                     style="direction: rtl; max-width: 100svw; max-height: 100svh;">
-                                    <app-dialog></app-dialog>
-                </div>
-            
+        const env = servicesProvider.getService(ConfigurationService).environment
+        if (env !== 'development') {
+            return html``
+        }
+        return html`
+            <div class="h-screen w-screen flex-col items-center justify-center  overflow-x-hidden"
+                 style="direction: rtl; max-width: 100svw; max-height: 100svh;">
+                <app-dialog></app-dialog>
+            </div>
+
         `
     }
 
