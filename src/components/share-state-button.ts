046bfc11041f5fd7b176154a70ab992920702fec
Chen Peleg <chenpeleg@gmail.com>
2026-03-17 10:58:51 +0200

    added shared state button

--- src/components/share-state-button.ts
+++ src/components/share-state-button.ts
@@ -1,35 +1,25 @@
 import {html, LitElement} from 'lit'
-import {customElement, state} from 'lit/decorators.js'
+import {customElement} from 'lit/decorators.js'
 import {globalStyleSheet} from '../styles/global-style-sheet.ts';
 import {Txt} from '../translations/translations.ts';
 import {globalStore} from '../store/Store.ts';
-import {ChildrenDisplayType} from '../models/AttendanceStore.ts';
-import gridImage from '../assets/svg/grid.svg'
-import listImage from '../assets/svg/list.svg'
-import {ActionType} from '../models/AppAction.ts';
+import copyImage from '../assets/svg/copy-content.svg'
 
 
 @customElement('share-state-button')
 export class ShareStateButton extends LitElement {
-    @state() private displayType: ChildrenDisplayType = globalStore.getState().childrenDisplayType;
 
 
     clickOption(): void {
-
-        globalStore.dispatch({
-            type: ActionType.changeListOrGrid,
-            payload:  this.displayType === ChildrenDisplayType.List ? ChildrenDisplayType.Grid : ChildrenDisplayType.List
-        })
+        const state = globalStore.getState()
+        console.log('state', state)
 
     }
 
 
     firstUpdated() {
         (this.shadowRoot as ShadowRoot).adoptedStyleSheets = [globalStyleSheet];
-        globalStore.subscribe((value) => {
-            this.displayType = value.childrenDisplayType;
-            this.requestUpdate();
-        })
+
     }
 
 
@@ -37,12 +27,12 @@ export class ShareStateButton extends LitElement {
         return html`
             <div class="h-14 w-full flex-row flex justify-between gap-5">
                     <span>
-                    ${Txt.displayLayout} 
+                    ${Txt.shareList} 
                     </span>
-                    <span>
+                <span>
                              <button @click=${() => this.clickOption()}
                                      class=" bg-secondary rounded-md  h-10 cursor-pointer flex flex-row px-3 justify-center items-center ">
-                        <img src="${this.displayType === ChildrenDisplayType.List ? listImage : gridImage}" class="w-7 h-7 app-icon"
+                        <img src="${copyImage}" class="w-7 h-7 app-icon"
                              alt="menu"/>
                     </button>
                     </span>
