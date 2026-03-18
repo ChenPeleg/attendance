c414b0d6ac0435cb1119aada278f9d7359372ba2
Chen Peleg <chenpeleg@gmail.com>
2026-03-18 10:38:44 +0200

    Implement initial state retrieval and byte code encoding in StateDecoderEncoderService

--- src/services/StateDecoderEncoder.service.ts
+++ src/services/StateDecoderEncoder.service.ts
@@ -1,11 +1,28 @@
 import {AbstractBaseService} from './provider/AbstractBaseService.ts';
 import {AttendanceStoreShare} from '../models/AttendanceStore.ts';
 import {ServicesResolver} from './provider/ServiceResolverClass.ts';
+import {StoreService} from './Store.service.ts';
 
 export class StateDecoderEncoderService extends AbstractBaseService {
     constructor(provider: ServicesResolver) {
         super(provider);
     }
+
+    private getInitialState()  {
+        const initialState =  this.servicesResolver.getService(StoreService).initialState
+        return initialState.attendance;
+    }
+
+    byteCodeEncode (state: AttendanceStoreShare): string {
+        const initialState =  this.getInitialState();
+        if (initialState.length  > 10) {
+            return  '10'
+        }
+        return  `${state.attendance.length}`
+
+    }
+
+
 
 
     public encode(state: AttendanceStoreShare): string {
