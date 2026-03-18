4619a42603e486ba94f5bc69aee6687c100db79d
Chen Peleg <chenpeleg@gmail.com>
2026-03-18 10:54:54 +0200

    Implement initial state retrieval and byte code encoding in StateDecoderEncoderService

--- src/services/StateDecoderEncoder.service.ts
+++ src/services/StateDecoderEncoder.service.ts
@@ -2,27 +2,39 @@ import {AbstractBaseService} from './provider/AbstractBaseService.ts';
 import {AttendanceStoreShare} from '../models/AttendanceStore.ts';
 import {ServicesResolver} from './provider/ServiceResolverClass.ts';
 import {StoreService} from './Store.service.ts';
+import {ChildStatus} from '../models/ChildStatus.ts';
+import {PresentToday} from '../models/presentToday.ts';
+
+export enum ChildByteStatus {
+    PresentAndNotChecked = 1, PresentAndChecked = 2, PresentAndUnchecked = 3, NotPresent = 4,
+}
 
 export class StateDecoderEncoderService extends AbstractBaseService {
     constructor(provider: ServicesResolver) {
         super(provider);
     }
 
-    private getInitialState()  {
-        const initialState =  this.servicesResolver.getService(StoreService).initialState
-        return initialState.attendance;
-    }
-
-    byteCodeEncode (state: AttendanceStoreShare): string {
-        const initialState =  this.getInitialState();
-        if (initialState.length  > 10) {
-            return  '10'
+    byteCodeEncode(state: AttendanceStoreShare): string {
+        const allChildren = this.getChildrenList();
+        const childrenByte: { id: number, childByteStatus: ChildByteStatus }[] = []
+        for (const child of allChildren) {
+            const childFromState = state.attendance.find(c => c.id === child.id);
+            if (childFromState) {
+                childrenByte.push({
+                    id: parseInt(child.id),
+                    childByteStatus: this.getChildByteStatus(childFromState)
+                })
+            } else {
+                childrenByte.push({
+                    id: parseInt(child.id),
+                    childByteStatus: ChildByteStatus.NotPresent
+                })
+            }
         }
-        return  `${state.attendance.length}`
+
+
+        return `${state.attendance.length}`
     }
-
-
-
 
     public encode(state: AttendanceStoreShare): string {
         const jsonState = JSON.stringify(state);
@@ -42,5 +54,25 @@ export class StateDecoderEncoderService extends AbstractBaseService {
             console.error('Failed to decode state', e);
             throw e;
         }
+    }
+
+    private getChildrenList() {
+        const initialState = this.servicesResolver.getService(StoreService).initialState
+        return initialState.attendance;
+    }
+
+    private getChildByteStatus(childStatus: ChildStatus): ChildByteStatus {
+        // return the enum
+        switch (childStatus.presentToday) {
+            case PresentToday.No:
+                return ChildByteStatus.NotPresent;
+            case PresentToday.Yes:
+            default:
+                return childStatus.checkedIn ? ChildByteStatus.PresentAndChecked : ChildByteStatus.PresentAndUnchecked;
+
+
+        }
+
+
     }
 }
