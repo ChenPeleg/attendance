020414b0372ef65ed3f8afc830a70657ee828b49
Chen Peleg <chenpeleg@gmail.com>
2026-03-19 10:25:30 +0200

    Enhance SharedDataDialog to display attendance count and last updated time

--- src/models/child.model.ts
+++ src/models/child.model.ts
@@ -4,7 +4,7 @@ import {Gender} from './Gender.ts';
 import {RangedId} from './RangedId.ts';
 
 export type ChildModel = {
-                             id: RangedId; name: string; school: SchoolType; schoolClass: SchoolClass; boyOrGirl: Gender
+                             id: RangedId; name: string; school: SchoolType; schoolClass: SchoolClass; boyOrGirl: Gender;
                              manuallyAdded?: false; onlySchoolBus?: boolean;
                          } | {
                              id: RangedId; name: string; manuallyAdded: true;
